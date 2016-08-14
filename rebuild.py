5c8093e6ad0655350738a3d360d4030c090c653f
alingse <alingse@foxmail.com>
2016-08-15 00:43:45 +0800

    add:添加渲染LIST页

--- rebuild.py
+++ rebuild.py
@@ -13,26 +13,31 @@ def load_meta(article,home):
 
     with open(fpath,'r') as f:
         #load yaml meta
-        content = f.read().strip()        
+        content = f.read().decode('utf-8').strip()
         if not content.startswith('---'):
             exce = 'this file format error {}'.format(article)
             raise Exception(exce)
+
         sk = content.find('---')
         ek = content.find('---',sk+3)
-        meta = yaml.load(content[sk+3:ek].strip())
+        info = yaml.load(content[sk+3:ek].strip())
 
         #load names
+        article = article.decode('utf-8')
         namelst = article.split('.')
+
+        meta = {}
+
         meta['id'] = namelst.pop(0)
-        meta['title'] = namelst.pop(0)
+        meta['type'] = namelst.pop(0)
         if article.endswith('.raw.md'):
             meta['tranraw'] =  True
             namelst.pop()
         meta['title'] = '.'.join(namelst)
 
         #rename
-        meta['author'] = meta[u'原作者']
-        meta['link'] = meta[u'来源']
+        meta['author'] = info[u'原作者']
+        meta['link'] = info[u'来源']
         meta['file'] = article
 
         return meta
@@ -42,17 +47,31 @@ def render(fname,home,**kwargs):
     fin = os.path.join(home,path_template,fname)
     fout = os.path.join(home,fname)
 
-    with open(fins,'r') as f:
-        template = Template(f.read())    
+    with open(fin,'r') as f:
+        content = f.read().decode('utf-8')
+        template = Template(content)
         out = template.render(**kwargs)
     with open(fout,'w') as f:
-        f.write(out)
+        f.write(out.encode('utf-8'))
 
 
 def main(home):
-    pass
-    
-    
+    #load metas
+    reposts = []
+    tranlist = []
+    for fname in os.listdir(home):
+        fid = fname.split('.',1)[0]
+        if not fid.isdigit():
+            continue
+        meta = load_meta(fname,home)
+        if meta['type'] == 'tran':
+            if not meta.get('tranraw'):
+                tranlist.append(meta)
+        elif meta['type'] == 'repost':
+            reposts.append(meta)
+
+    renderfile = 'LIST.md'
+    render(renderfile,home,reposts=reposts,tranlist=tranlist)
 
 
 if __name__ == '__main__':
